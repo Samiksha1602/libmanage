e237a4e4add06e9110cc3d20949009b0bf197621
Samiksha <csamiksharaul@gmail.com>
2022-04-12 23:17:51 +0530

    second commit

--- server.js
+++ server.js
@@ -48,17 +48,17 @@ app.use(passport.initialize());
 app.use('/api/books',bookRouter);
 app.use('/api/users',userRouter);
 app.use('/api/issues',issueRouter);
-
+console.log(__dirname)
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('/build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
   }
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started running on port ${port}`));
